4fb637708c3831b3b97362fed02a84f0919d12c7
gancheev <insanee8@gmail.com>
2015-01-09 10:03:26 -0800

    loadCategories update

---
+++ raw/js/controllers/CategoriesController.js
@@ -0,0 +1,15 @@
+﻿app.controller('CategoriesController', ['$scope', 'categoriesData', function ($scope, categoriesData) {
+    function loadCategories(params) {
+        var params = params || {};
+            categoriesData.getCategories()
+        .$promise
+        .then(function (data) {
+            $scope.categories = data;
+        }); 
+    }
+    loadCategories();
+    $scope.categoryClicked = function categoryClicked(category) {
+        filter.filterByCategory(category);
+        loadCategories();
+    }
+}])
